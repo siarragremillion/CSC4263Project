b363ece557b56b52928f6e3415912aa261fa66db
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-03-11 21:50:08 -0600

    - Added Game Project and added some UI things
    Just clone the repo and open the whole repo in unity

---
+++ Assets/Scripts/Rocky.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocky : MonoBehaviour
+{
+
+    public int health { get; set; } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
