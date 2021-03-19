c6005efa0b308846acdeeb78315b4ab6e24246a6
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-03-18 22:20:40 -0500

    - adding some random code stuff

--- Assets/Scripts/Rocky.cs
+++ Assets/Scripts/Rocky.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 public class Rocky : MonoBehaviour
 {
 
-    public int health { get; set; } 
+    public int Health { get; set; }
+
+    public int MaxHealth { get; set; }
+
+    public int Crystals { get; set; }
+
+    public int MaxCrystals { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 3;
+        MaxHealth = 3;
+        Crystals = 0;
+        Health = MaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Health <= 0)
+        {
+            // DEAD
+            // Implement Game Over
+        }
+    }
+
+    public void PickUpHeart()
+    {
+        if (Health < MaxHealth)
+        {
+            Health++;
+        }
+    }
+
+    public void EatFood(int HealthAmount)
+    {
+        Health += HealthAmount;
+        if (Health > MaxHealth)
+        {
+            Health = MaxHealth;
+        }
+    }
+
+    public void PickUpCrystal()
+    {
+        if (Crystals < MaxCrystals)
+        {
+            Crystals++;
+        }
     }
 }
