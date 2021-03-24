5314ca809f3bed91b939c9749807d017c807f8bc
Talia Teplitzky <talia_tep@yahoo.com>
2021-03-24 08:47:32 -0500

    fixed bullet velocity by setting gravity scale to 0

--- Assets/Scripts/BulletMover.cs
+++ Assets/Scripts/BulletMover.cs
@@ -6,13 +6,11 @@ public class BulletMover : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
-    // private Vector2 lastPos;
-    //public GameObject Rocky; 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Rocky rocky = GameObject.FindGameObjectWithTag("Player").GetComponent<Rocky>();
+        GameObject rocky = GameObject.FindGameObjectWithTag("Player");
         rb.velocity = rocky.GetComponent<PlayerMovement>().GetMovement() * speed;
     }
 }
