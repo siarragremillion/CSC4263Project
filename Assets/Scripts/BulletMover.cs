8848b37cbbd16ef5b515e712a1a264adddba4368
Abbie Crabtree <acrabt1@lsu.edu>
2021-03-24 20:40:23 -0500

    Fix Bullet Issue

--- Assets/Scripts/BulletMover.cs
+++ Assets/Scripts/BulletMover.cs
@@ -6,11 +6,20 @@ public class BulletMover : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
+    private Vector2 movement;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         GameObject rocky = GameObject.FindGameObjectWithTag("Player");
-        rb.velocity = rocky.GetComponent<PlayerMovement>().GetMovement() * speed;
+        movement = rocky.GetComponent<PlayerMovement>().GetMovement();
+        if(movement.x == 0 && movement.y == 0){
+            rb.velocity = -transform.up * speed;
+            Debug.Log("Test");
+        }
+        else{
+            rb.velocity = rocky.GetComponent<PlayerMovement>().GetMovement() * speed;
+            Debug.Log("Test 2");
+        }
     }
 }
