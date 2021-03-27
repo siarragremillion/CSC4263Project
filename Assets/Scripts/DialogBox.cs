cffcb481a4c5261c853769fd684032bee0b31d92
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-03-26 23:35:56 -0500

    added sample dialog and item selection text for vendor
    Need to create a system to select the item then buy said item.

---
+++ Assets/Scripts/DialogBox.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogBox : MonoBehaviour
+{
+    [SerializeField] Text dialogText;
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] GameObject itemSelector;
+    [SerializeField] List<Text> itemTexts;
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f/lettersPerSecond);
+        }
+
+        //EnableItemText(true);
+    }
+
+    public void EnableItemText(bool enabled)
+    {
+        itemSelector.SetActive(enabled);
+    }
+}
---
+++ Assets/Scripts/VendorSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendorSystem : MonoBehaviour
+{
+    [SerializeField] Rocky player;
+    [SerializeField] DialogBox dialogBox;
+
+    private void Start()
+    {
+        dialogBox.EnableItemText(false);
+        StartCoroutine(SetupVendor());
+    }
+
+    public IEnumerator SetupVendor()
+    {
+        yield return dialogBox.TypeDialog("Hey Rock, the names Paulie. What weapon do you want to upgrade?");
+        yield return new WaitForSeconds(1f/30);
+
+        ItemSelection();
+    }
+
+    void ItemSelection()
+    {
+        dialogBox.EnableItemText(true);
+    }
+}
