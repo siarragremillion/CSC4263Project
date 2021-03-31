a7cf72d954d2aa7448871745c0328ec4af544f2a
siarragremillion <55928548+siarragremillion@users.noreply.github.com>
2021-03-30 23:37:17 -0500

    Somewhat fixed enemy bounce back

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,9 +12,8 @@ public class EnemyAI : MonoBehaviour
     public float moveSpeed = 3f;
     public float detectDistance = 5f;
     [SerializeField] Transform target;
-
     private void Update(){
-        if(target != null){
+        if (target != null){
             /**
             ROTATE ENEMY was in video but we probably won't use, I just left it in case we want 
             like a worm enemy or something
@@ -25,7 +24,6 @@ public class EnemyAI : MonoBehaviour
             // float _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
 
             // transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, _angle + offset));
-
             transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
         }
     }
--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -6,18 +6,12 @@ public class EnemyDamage : MonoBehaviour
 {
     public int power = 1;
     public int damageRate = 10;
-    private Vector3 prevPos;
-    private Vector3 newPos;
-    private Vector3 direction;
 	[SerializeField] protected float hitCooldown = 2f;
     protected float currHitCoolDown = 2f;
-
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
     private void OnCollisionEnter2D(Collision2D other){
         if(other.collider.CompareTag("Player"))
         {
@@ -29,9 +23,11 @@ public class EnemyDamage : MonoBehaviour
             /* This bounces the enemy back by 1 in the y direction so it only works if rocky is attacking from above the enemy. 
              * If we can get his direction then we can do something similar to this to make him bounce back by one in the appropriate direction.
              */
+            Vector2 moveDirection = transform.position - other.transform.position;
+            Vector2 newDir = moveDirection.normalized;
             Vector3 temp = transform.position;
-            temp.x = transform.position.x;
-            temp.y = transform.position.y - 1f;
+            temp.x = transform.position.x + newDir.x;
+            temp.y = transform.position.y + newDir.y;
             transform.position = temp;
         }
     }
@@ -45,25 +41,20 @@ public class EnemyDamage : MonoBehaviour
             }
         }        
     }
-    private void BounceEnemy(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        /* This bounces the enemy back by 1 in the y direction so it only works if rocky is attacking from above the enemy. 
-             * If we can get his direction then we can do something similar to this to make him bounce back by one in the appropriate direction.
-             */
-        Vector3 temp = transform.position;
-        temp.x = transform.position.x;
-        temp.y = transform.position.y - 1f;
-        transform.position = temp;
-       
-    }
-    private void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Bullet")
+        if (other.tag == "Bullet")
         {
             Destroy(other.gameObject);
             gameObject.GetComponent<EnemyHealth>().HurtEnemy(GameObject.FindObjectOfType<Rocky>().gunPower);
-            //Vector2 moveDirection = transform.position - other.transform.position;
-            //GetComponent<Rigidbody2D>().AddForce(moveDirection.normalized *-500f, ForceMode2D.Impulse);
-            BounceEnemy(other);
+            Vector2 moveDirection = transform.position - other.transform.position;
+            Vector2 newDir = moveDirection.normalized;
+            Vector3 temp = transform.position;
+            temp.x = transform.position.x + newDir.x;
+            temp.y = transform.position.y + newDir.y;
+            transform.position = temp;
+
         }
     }
 
